fa45111422bd43a794f0476b910bdd9a4d85d510
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-06-22 18:20:17 +0900

    Style login form

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  Container,
+  Header,
+  Body,
+  Title,
+  Form,
+  Input,
+  Item,
+  Button,
+  Label
+} from "native-base";
+
 import firebase from "firebase";
 import {
   FIREBASE_API_KEY,
@@ -20,17 +32,66 @@ firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container style={styles.container}>
+      <Header>
+        <Body>
+          <Title>MUSICiAN</Title>
+        </Body>
+      </Header>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center"
+        }}
+      >
+        <Form>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              // onChangeText={email => this.setState({ email })}
+            />
+          </Item>
+
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              // onChangeText={password => this.setState({ password })}
+            />
+          </Item>
+
+          <Button
+            style={{ marginTop: 10 }}
+            full
+            rounded
+            success
+            // onPress={() => this.loginUser(this.state.email, this.state.password)}
+          >
+            <Text style={{ color: "white" }}>Login</Text>
+          </Button>
+
+          <Button
+            style={{ marginTop: 10 }}
+            full
+            rounded
+            primary
+            // onPress={() => this.signUpUser(this.state.email, this.state.password)}
+          >
+            <Text style={{ color: "white" }}>Sign Up</Text>
+          </Button>
+        </Form>
+      </View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "#fff"
   }
 });
