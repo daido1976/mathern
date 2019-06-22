27a8b8c07a422b71f25947915ec05a378286e659
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-06-22 17:55:19 +0900

    Initialize firebase

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID
+} from "react-native-dotenv";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
