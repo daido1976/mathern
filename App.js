b17e006354064b0b1f54bd5e191512a76ae7d8bb
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-06-22 17:56:02 +0900

    Format code

--- App.js
+++ App.js
@@ -29,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
