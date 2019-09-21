da828dc290c7dea411d186e019044b7291ee8f77
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-09-21 17:27:54 +0900

    Prepare saveName

--- src/screens/EditNameScreen.js
+++ src/screens/EditNameScreen.js
@@ -1,10 +1,21 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button } from "react-native";
 
 export const EditNameScreen = props => {
+  const [value, onChangeText] = useState(props.navigation.state.params.name);
+
+  const saveName = () => {
+    props.navigation.goBack();
+  };
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>{props.navigation.state.params.name}</Text>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <Button title="保存する" onPress={saveName} />
     </View>
   );
 };
