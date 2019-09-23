121f85992f5ce402e207dd5cf6944abe1d0ccee2
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-09-23 17:55:14 +0900

    Side by side in a row direction

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -1,11 +1,54 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Avatar } from "react-native-elements";
 
 export default class DiscoverScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Discover</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap"
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "lightgray",
+            alignItems: "center"
+          }}
+        >
+          <Avatar
+            rounded
+            size="xlarge"
+            source={{
+              uri:
+                "https://facebook.github.io/react-native/docs/assets/favicon.png"
+            }}
+          />
+          <View>
+            <Text>30歳 埼玉</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "skyblue",
+            alignItems: "center"
+          }}
+        >
+          <Avatar
+            rounded
+            size="xlarge"
+            source={{
+              uri:
+                "https://facebook.github.io/react-native/docs/assets/favicon.png"
+            }}
+          />
+          <View>
+            <Text>30歳 埼玉</Text>
+          </View>
+        </View>
       </View>
     );
   }
