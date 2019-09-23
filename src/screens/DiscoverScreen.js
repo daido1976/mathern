4a3328767810fc9b68eb363ec168e0b33760ea45
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-09-23 15:59:35 +0900

    Move signOut button to ProfileStack
    buttonStyle props
    ref. https://facebook.github.io/react-native/docs/view-style-props

--- src/screens/DiscoverScreen.js
+++ src/screens/DiscoverScreen.js
@@ -1,28 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Button } from "native-base";
-import firebase from "firebase";
 
 export default class DiscoverScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Discover</Text>
-        <Button
-          style={{ marginTop: 10 }}
-          full
-          rounded
-          danger
-          onPress={this._signOutAsync}
-        >
-          <Text style={{ color: "white" }}>Sign Out</Text>
-        </Button>
       </View>
     );
   }
-
-  _signOutAsync = async () => {
-    await firebase.auth().signOut();
-    this.props.navigation.navigate("Auth");
-  };
 }
