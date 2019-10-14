58f361fb8b9c6498269e55ae080fe6d3c163804f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-10-14 19:33:53 +0900

    Add matches user list to MessageScreen

--- src/screens/MessageScreen.tsx
+++ src/screens/MessageScreen.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { ListItem } from "react-native-elements";
 
 export const MessageScreen = () => {
+  const list = [
+    {
+      name: "daido 28歳 大阪",
+      avatar_url:
+        "https://facebook.github.io/react-native/docs/assets/favicon.png",
+      subtitle: "メッセージが届いています"
+    },
+    {
+      name: "shota 30歳 東京",
+      avatar_url:
+        "https://facebook.github.io/react-native/docs/assets/favicon.png",
+      subtitle: "メッセージが届いています"
+    }
+  ];
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Message</Text>
+    <View>
+      {list.map((l, i) => (
+        <ListItem
+          key={i}
+          leftAvatar={{ source: { uri: l.avatar_url } }}
+          title={l.name}
+          titleStyle={{ fontSize: 15 }}
+          subtitle={l.subtitle}
+          subtitleStyle={{ color: "grey", marginTop: 5, fontSize: 12 }}
+          bottomDivider
+        />
+      ))}
     </View>
   );
 };
