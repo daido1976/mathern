7e229b868ef46cc39c6cb80b9679b176fb164b3d
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-10-14 13:09:18 +0900

    TabNavigator に Liked を追加
    とりあえず、DiscoverScreen と同じ表示をさせるところまで

---
+++ src/containers/Liked.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import firebase from "firebase";
+import "firebase/firestore";
+import { DiscoverScreen } from "../screens/DiscoverScreen";
+
+export const Liked = props => {
+  const [users, setUsers] = useState([]);
+  const [currentUserId, setCurrentUserId] = useState();
+
+  const handlePress = user => () => {
+    props.navigation.navigate("ShowProfile", {
+      user: {
+        id: user.id,
+        avatarUrl: user.avatarUrl,
+        name: user.name,
+        address: user.address
+      },
+      currentUserId,
+      profileType: "Liked"
+    });
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => setCurrentUserId(user.uid));
+    getAllUser();
+  }, []);
+
+  const getAllUser = async () => {
+    const usersSnapshot = await firebase
+      .firestore()
+      .collection("users")
+      .get();
+
+    usersSnapshot.forEach(doc => {
+      console.log(doc.id, " => ", doc.data());
+    });
+
+    const users = usersSnapshot.docs.map(doc => {
+      return {
+        id: doc.id,
+        age: 20,
+        name: doc.data().name,
+        address: doc.data().address,
+        avatarUrl: doc.data().avatarUrl
+      };
+    });
+
+    setUsers(users);
+  };
+
+  return <DiscoverScreen data={users} onPress={handlePress}></DiscoverScreen>;
+};
