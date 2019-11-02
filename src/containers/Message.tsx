24a0104a34a71fa29f68f5079dac20b610ee3ee4
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-11-02 14:47:56 +0900

    Impl Message container

--- src/containers/Message.tsx
+++ src/containers/Message.tsx
@@ -4,5 +4,38 @@ import "firebase/firestore";
 import { MessageScreen } from "../screens/MessageScreen";
 
 export const Message = props => {
-  return <MessageScreen></MessageScreen>;
+  const [users, setUsers] = useState([]);
+  const [currentUserId, setCurrentUserId] = useState();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => setCurrentUserId(user.uid));
+    getMatchUsers();
+  }, [currentUserId]);
+
+  const getMatchUsers = async () => {
+    if (!currentUserId) {
+      return null;
+    }
+
+    const usersSnapshot = await firebase
+      .firestore()
+      .collection("users")
+      .where("matches", "array-contains", currentUserId)
+      .get();
+
+    const users = usersSnapshot.docs
+      .map(doc => {
+        return {
+          id: doc.id,
+          age: 20,
+          name: doc.data().name,
+          address: doc.data().address,
+          avatarUrl: doc.data().avatarUrl
+        };
+      })
+
+    setUsers(users);
+  };
+
+  return <MessageScreen users={users}></MessageScreen>;
 };
